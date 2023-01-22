1d3a87da245c02cfea615181dda52210e82b964d
Kirill <kir.molokoff@yandex.ru>
2023-01-22 17:18:26 +0900

    fin1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+int weekday; 
+Console.Write("Введите номер дня (1-7): "); 
+weekday = Convert.ToInt32(Console.ReadLine()); 
+switch (weekday) 
+    { 
+        case [1,5]: 
+            Console.WriteLine("Рабочий день"); 
+            break; 
+        case [6,7]: 
+            Console.WriteLine("Выходной");
+        default: 
+            Console.WriteLine("неправильный номер"); 
+            break; 
+    } 
